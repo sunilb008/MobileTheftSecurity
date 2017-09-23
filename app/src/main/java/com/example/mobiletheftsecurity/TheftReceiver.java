38257bb283bd0e5c17cbb60e248aad4dd6b2ccce
Sunil <sunilbijjaragi@gmail.com>
2017-09-23 14:37:35 +0530

    Hello

--- app/src/main/java/com/example/mobiletheftsecurity/TheftReceiver.java
+++ app/src/main/java/com/example/mobiletheftsecurity/TheftReceiver.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
@@ -19,6 +21,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.provider.Settings;
 import android.sax.StartElementListener;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
@@ -28,44 +31,44 @@ public class TheftReceiver extends BroadcastReceiver {
 	String num;
 	SharedPreferences sp;
 	TelephonyManager tm;
-	String simserial1,simserial2="";
-	String network="";
+	String simserial1, simserial2 = "";
+	String network = "";
 	LocationManager lm;
 	Context c;
 	double c_lat;
 	double c_long;
-	
+
 	String address;
-	
-	LocationListener ll=new LocationListener() {
-		
+
+	LocationListener ll = new LocationListener() {
+
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onProviderEnabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onProviderDisabled(String provider) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onLocationChanged(Location location) {
 			// TODO Auto-generated method stub
-			c_lat=location.getLatitude() ;
+			c_lat = location.getLatitude();
 
-			c_long=location.getLongitude();
+			c_long = location.getLongitude();
 			getAddress();
-			Toast.makeText(c,c_lat+","+c_long,Toast.LENGTH_LONG).show();
-			Intent in=new Intent(c,ChangePassword.class);
+			Toast.makeText(c, c_lat + "," + c_long, Toast.LENGTH_LONG).show();
+			Intent in = new Intent(c, ChangePassword.class);
 			in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			c.startActivity(in);
 		}
@@ -73,13 +76,23 @@ public class TheftReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-	    // TODO Auto-generated method stub
-        c=context;
-		Toast.makeText(context,"boot received",Toast.LENGTH_LONG).show();
-        turnGPSOn(context);
-		tm=(TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		lm=(LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, ll);
+		// TODO Auto-generated method stub
+		c = context;
+		Toast.makeText(context, "boot received", Toast.LENGTH_LONG).show();
+		turnGPSOn(context);
+		tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+		if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
 		lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0, ll);
 		lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -89,7 +102,7 @@ public class TheftReceiver extends BroadcastReceiver {
 		mdb=new MyDataBase(context);
 		mdb.open();
 		Cursor c=mdb.retrieveData();
-	    sp=context.getSharedPreferences("simDetails",0);
+		sp=context.getSharedPreferences("simDetails",0);
 		simserial2=sp.getString("simno",null);
 
 		while (c.moveToNext()) {
